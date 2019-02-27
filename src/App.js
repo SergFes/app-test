b34d5ae24bf9d6edd093104929fe7bee95b9c6e5
Sergey <you@example.com>
2019-02-28 00:43:04 +0300

    deploy netlify

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Изучить React
           </a>
         </header>
       </div>
