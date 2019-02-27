57b34df2850e1e5803e2f4d9845c6b636bea6889
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-27 20:50:10 +0000

    Deploy SergFes/app-test to github.com/SergFes/app-test.git:gh-pages

---
+++ precache-manifest.14eaae9638db1b43f4d5ca21bb2a0dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2cba84c5edffd2d267",
+    "url": "/static/css/main.6f6c44e8.chunk.css"
+  },
+  {
+    "revision": "ec2cba84c5edffd2d267",
+    "url": "/static/js/main.ec2cba84.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24372a703ed6f8495d8163daf466efed",
+    "url": "/index.html"
+  }
+];
