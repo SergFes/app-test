22fe321b073ecf237c5a29e015226bfa03c6be29
Sergey <you@example.com>
2019-02-27 22:15:33 +0300

    add failed test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const sum = (a,b)=>a+b;
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('function sum', () => {
+    expect(sum(2,3)).toEqual(2)
 });
